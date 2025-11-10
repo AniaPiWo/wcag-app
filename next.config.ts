ea0cf74f856685db84453e6f025b013e9930322a
AniaPiWo <snowflake@wp.pl>
2025-11-10 13:09:55 +0100

    Revert "performance opt"
    This reverts commit ea93ba39448bc02ffcd84cd7019c88a5ed707d3b.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-// 🚀 PERFORMANCE: Bundle analyzer
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 const nextConfig: NextConfig = {
   // Konfiguracja dla zewnętrznych pakietów
   serverExternalPackages: ['playwright-core', 'playwright'],
@@ -29,46 +23,7 @@ const nextConfig: NextConfig = {
   // Konfiguracja dla API Routes
   experimental: {
     optimizePackageImports: ['react-icons', 'framer-motion'],
-    // 🚀 PERFORMANCE: Włącz turbo mode dla szybszego buildu
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
-  
-  // 🚀 PERFORMANCE: Webpack optimizations
-  webpack: (config, { dev }) => {
-    // Optymalizacje tylko dla produkcji
-    if (!dev) {
-      // Tree shaking dla dużych bibliotek
-      config.optimization.usedExports = true;
-      config.optimization.sideEffects = false;
-      
-      // Chunk splitting dla lepszego cache'owania
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-          },
-          wcag: {
-            test: /[\\/]src[\\/]lib[\\/]wcag_checklist[\\/]/,
-            name: 'wcag-checklist',
-            chunks: 'all',
-            priority: 10,
-          },
-        },
-      };
-    }
-    
-    return config;
   },
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
