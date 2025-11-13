75a7df8da31766e8910a243dee8fb8846acb908e
AniaPiWo <snowflake@wp.pl>
2025-11-13 15:21:56 +0100

    ui

--- src/components/landing/Hero/Hero.tsx
+++ src/components/landing/Hero/Hero.tsx
@@ -86,7 +86,6 @@ export const Hero = () => {
       )}
 
       <div className={styles.top}>
-        <div className={styles.tagline}>Strony bez barier</div>
         <h1 className={styles.title}>
           Popraw dostępność, zwiększ sprzedaż i widoczność – dzięki WCAG 2.2
         </h1>
